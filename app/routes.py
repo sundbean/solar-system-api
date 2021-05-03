2a2fc4f0c5efb45d9caffbc8d7859308e2dff51e
Whit Sundby <wksundby@gmail.com>
2021-05-03 09:07:23 -0700

    a bunch of changes from friday roundtable, oops

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,19 @@
-from flask import Blueprint
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, make_response, request, jsonify
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def handle_planet():
+    
+    request_body = request.get_json()
+
+    new_planet - Planet(name=request_body['name'], description=request_body['description'], size=request_body['size'])
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} has been created", 201)
+
 
