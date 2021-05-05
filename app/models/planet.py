ae5f525dd99f306de4b8d9b01d3030cf1341cd1a
Whit Sundby <wksundby@gmail.com>
2021-05-04 19:40:00 -0700

    adds day_length, year_length, and average_temp to Planet model

--- app/models/planet.py
+++ app/models/planet.py
@@ -4,4 +4,7 @@ class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
+    day_length = db.Column(db.Float)
+    year_length = db.Column(db.Float)
+    average_temp = db.Column(db.Float)
     size = db.Column(db.Float)
