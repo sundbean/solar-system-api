dda3f2df769c8177a40d7b2a89b2d1ea917716ef
Whit Sundby <wksundby@gmail.com>
2021-05-04 19:37:18 -0700

    adds 'GET' method functionality to /planets route

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,31 @@ from flask import Blueprint, make_response, request, jsonify
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["POST"])
+@planets_bp.route("", methods=["POST", "GET"])
 def handle_planet():
+
+    if request.method == "POST":
     
-    request_body = request.get_json()
+        request_body = request.get_json()
 
-    new_planet = Planet(name=request_body['name'], description=request_body['description'], size=request_body['size'])
+        new_planet = Planet(name=request_body['name'], description=request_body['description'], size=request_body['size'])
 
-    db.session.add(new_planet)
-    db.session.commit()
+        db.session.add(new_planet)
+        db.session.commit()
 
-    return make_response(f"Planet {new_planet.name} has been created", 201)
+        return make_response(f"Planet {new_planet.name} has been created", 201)
 
+    elif request.method == "GET":
 
+        planets = Planet.query.all()
+        planets_response = []
+
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "size": planet.size
+            })
+
+            return jsonify(planets_response)
