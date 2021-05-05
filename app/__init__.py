0e05c4a85c356573948c28d3e1c95361c3defad8
Whit Sundby <wksundby@gmail.com>
2021-05-04 22:02:21 -0700

    modifies create_app() in __init__.py to handle test mode

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()   # loads values from our .env file so that the os module is able to see them
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    if not test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True # turns testing mode on
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app, db)
